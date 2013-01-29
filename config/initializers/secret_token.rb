66a029624d5e37fe4b1f9786e18e4d5230134520
Jonathan Romero <jrarellanes@hotmail.com>
2013-01-29 13:14:51 -0700

    Primer Avance, modulo para consulta de sitios ABC catalogos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TouristKiosk::Application.config.secret_token = '1a8bede258fe8cf2a94294ccfe0a4139a0943004b84ee4e0741a767874dd0b5369d407a9cb94ec92efe2ba89ce09c4653eec3b442c69dbd1c311546ba57fa056'
